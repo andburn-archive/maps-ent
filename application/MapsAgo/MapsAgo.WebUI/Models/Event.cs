ef6e9d6998243566cd9faf854485e5f6cc1b2a92
Andrew Burnett <andrew.alex.burnett@gmail.com>
2014-04-11 23:32:04 +0100

    Add basic models

---
+++ application/MapsAgo/MapsAgo.WebUI/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapsAgo.WebUI.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ application/MapsAgo/MapsAgo.WebUI/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapsAgo.WebUI.Models
+{
+    public class Location
+    {
+        // TODO float/double best for lat/long
+        public double latitude { get; set; }
+        public double longitdude { get; set; }
+        public string name { get; set; }
+    }
+}
