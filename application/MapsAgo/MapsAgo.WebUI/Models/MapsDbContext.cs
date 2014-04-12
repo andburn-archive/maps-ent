83ae9884efb448a6ff5bc4e6af0b447685ed1484
Andrew Burnett <andrew@nospam.forme>
2014-04-12 12:12:47 +0100

    Add DbContext and Connect String

---
+++ application/MapsAgo/MapsAgo.WebUI/Models/MapsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MapsAgo.WebUI.Models
+{
+    public class MapsDbContext : DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
