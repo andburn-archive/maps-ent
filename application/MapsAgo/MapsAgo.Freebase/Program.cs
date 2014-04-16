7537a335b31516938f334a351d5ae160d0997417
Andrew Burnett <andrew@nospam.forme>
2014-04-16 22:01:15 +0100

    Try out FreebaseAPI

---
+++ application/MapsAgo/MapsAgo.Freebase/Program.cs
@@ -0,0 +1,31 @@
+﻿using Google.Apis.Freebase.v1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapsAgo.Freebase
+{
+    class Program {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Freebase API Sample");
+            Console.WriteLine("====================");
+
+            // Create the service.
+            var service = new FreebaseService(new FreebaseService.Initializer
+            {
+                ApplicationName = "Freebase Sample",
+                ApiKey = "AIzaSyAq8f-ZUQUre00arIXI5hDl41XjaSPFqVI",
+            });
+
+            String query = "[{\"id\":null,\"name\":null,\"type\":\"/astronomy/planet\"}]";
+            var resquest = service.Search();
+            ServiceRequest request = service.Search();
+            string response = request.Fetch();
+            Console.WriteLine(response);
+        }
+    }
+}
